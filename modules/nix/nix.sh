86390d4baf2966706cc74f3b1677cb72faf41f89
Justin Pierro <jmpierro0513@gmail.com>
2026-05-21 16:44:52 -0400

    Remove unused dependency from units

--- modules/nix/nix.sh
+++ modules/nix/nix.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
-MODULE_DIR="$(dirname "$0")"
+mkdir -p /nix
+mkdir -p /var/lib/nix
 
+MODULE_DIR="$(dirname "$0")"
 cp "$MODULE_DIR/units/nix.mount" /etc/systemd/system/nix.mount
 cp "$MODULE_DIR/units/nix-install.service" /etc/systemd/system/nix-install.service
 
 mkdir -p /etc/systemd/system/local-fs.target.wants
-mkdir -p /etc/systemd/system/multi-user.target.wants
-
 ln -sf /etc/systemd/system/nix.mount \
     /etc/systemd/system/local-fs.target.wants/nix.mount
+
+mkdir -p /etc/systemd/system/multi-user.target.wants
 ln -sf /etc/systemd/system/nix-install.service \
     /etc/systemd/system/multi-user.target.wants/nix-install.service
